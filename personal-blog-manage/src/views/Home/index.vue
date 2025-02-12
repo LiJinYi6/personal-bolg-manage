03a298f800114feb2c7e814bc6eaf31b4982400b
LJY <3203345259@qq.com>
2025-02-12 23:10:55 +0800

    样式

--- personal-blog-manage/src/views/Home/index.vue
+++ personal-blog-manage/src/views/Home/index.vue
@@ -7,14 +7,48 @@
             </el-space>
         </div>
         <div class="centerPart">
-            展示部分
+            
+                <div class="echarts">
+                    
+                        <div class="visitCount">
+                            <el-space :size="600">
+                                <h3 style="display:inline">访问量</h3>
+                            <div style="display:inline">
+                                <el-button>近一周</el-button>
+                                <el-button>近一月</el-button>
+                                <div class="charts" ref="charts1"></div>
+                            </div>
+                            </el-space>
+                            
+                        </div>
+                        <div class="dataView">
+                            <el-space :size="580">
+                                <h3 style="display:inline">数据检测</h3>
+                            <div style="display:inline">
+                                <el-button>近一周</el-button>
+                                <el-button>近一月</el-button>
+                            </div>
+                            </el-space>
+                        </div>
+                  
+                    
+                </div>
+                <div class="comments">
+
+                </div>
+            
+            
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+import * as echarts from 'echarts'
 import card from '../../components/card.vue';
 import { overList } from '../../utils/overview';
+let charts1=ref()
+let visitCharts=echarts.init(charts1.value)
 </script>
 
 <style scoped lang="less">
@@ -29,16 +63,54 @@ import { overList } from '../../utils/overview';
     }
     .cardPart{
         flex:1.5;
-        border: 1px black solid;
+        
         display: flex;
         flex-direction: row;
         align-items: center;
         justify-content: center;
-       
     }
     .centerPart{
         flex:5;
-        border: 1px black solid;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        .echarts{
+            width: 100%;
+            height: 100%;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            margin-right: 10px;
+            flex:2;
+            
+            .visitCount{
+                background: white;
+                height: 49%;
+                width: 100%;
+                padding:10px;
+                border-radius: 8px;
+            }
+            .dataView{
+                
+                background: white;
+                height: 49%;
+                width: 100%;
+                border-radius: 8px;
+                padding:10px;
+            }
+        }
+        .comments{
+            background-color: white;
+            width: 100%;
+            height: 100%;
+            margin-left: 10px;
+            padding:5px;
+            border-radius: 8px;
+            flex:1;
+            
+        }
     }
 }
 
